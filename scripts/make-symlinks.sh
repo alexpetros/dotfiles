a5fae0c2101e3db20a5454532a21c7f5cf3973d8
Alexander Petros <apetros15@gmail.com>
2021-08-05 12:40:32 -0400

    Split PATH and non-interactive aliases into .zshenv

--- scripts/make-symlinks.sh
+++ scripts/make-symlinks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # create-symlinks.sh
-# 
+#
 # Create symlinks from expeted dotfile locations to this repo
 # This should only be run by the Makefile, from the repository root
 
@@ -12,6 +12,7 @@ ln_flags='-sv'
 ln $ln_flags $repo_dir/vim/vimrc ~/.vimrc
 ln $ln_flags $repo_dir/tmux/tmux.conf ~/.tmux.conf
 ln $ln_flags $repo_dir/zsh/zshrc ~/.zshrc
+ln $ln_flags $repo_dir/zsh/zshenv ~/.zshenv
 ln $ln_flags $repo_dir/latex/latexmkrc ~/.latexmkrc
 
 echo 'make-symlinks executed succesfully'
