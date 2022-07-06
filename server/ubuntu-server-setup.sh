1604b3895319ba7c6a0a4296171a39d508819b23
Alexander Petros <apetros15@gmail.com>
2022-07-05 23:10:10 -0400

    Add PM2 to the node installation

--- server/ubuntu-server-setup.sh
+++ server/ubuntu-server-setup.sh
@@ -7,21 +7,22 @@ usermod -aG sudo $USERNAME
 rsync --archive --chown=$USERNAME:$USERNAME ~/.ssh /home/$USERNAME
 
 # Install basic packages
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
 apt-get -y update
-apt-get -y install nginx make fzf nodejs
+apt-get -y install nginx make fzf
 
 # Verify that nginx is serving on port 80
 systemctl status nginx
 curl localhost:80 2>/dev/null | grep nginx > /dev/null
 
-# Install node 18.x
-sudo apt-get install -y nodejs
-
 ufw allow 'OpenSSH'
 ufw allow 'Nginx Full'
 ufw --force enable
 ufw status
+
+# Install node 18.x
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+apt-get install -y nodejs
+npm install -g pm2
 
 # Login to $USERNAME and install dotfiles
 sudo -i -u $USERNAME bash << EOF
